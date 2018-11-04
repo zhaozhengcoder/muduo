6743060b7265aec52c978cc3639d8ea952516ccf
赵政 <www.a821400729@qq.com>
2018-11-04 12:52:13 +0800

    add readme

--- muduo_tutorial/recipes/reactor/s04/Acceptor.cc
+++ muduo_tutorial/recipes/reactor/s04/Acceptor.cc
@@ -7,46 +7,44 @@
 
 #include "Acceptor.h"
 
-#include "logging/Logging.h"
 #include "EventLoop.h"
 #include "InetAddress.h"
 #include "SocketsOps.h"
+#include "logging/Logging.h"
 
 #include <boost/bind.hpp>
 
 using namespace muduo;
 
-Acceptor::Acceptor(EventLoop* loop, const InetAddress& listenAddr)
-  : loop_(loop),
-    acceptSocket_(sockets::createNonblockingOrDie()),
-    acceptChannel_(loop, acceptSocket_.fd()),     //创建一个channel的对象
-    listenning_(false)
+Acceptor::Acceptor(EventLoop *loop, const InetAddress &listenAddr)
+        : loop_(loop), acceptSocket_(sockets::createNonblockingOrDie()),
+        acceptChannel_(loop, acceptSocket_.fd()), //创建一个channel的对象
+        listenning_(false) 
 {
-  acceptSocket_.setReuseAddr(true);
-  acceptSocket_.bindAddress(listenAddr);
-  acceptChannel_.setReadCallback(boost::bind(&Acceptor::handleRead, this));
+    acceptSocket_.setReuseAddr(true);
+    acceptSocket_.bindAddress(listenAddr);
+    acceptChannel_.setReadCallback(boost::bind(&Acceptor::handleRead, this));
 }
 
-void Acceptor::listen()
+void Acceptor::listen() 
 {
-  loop_->assertInLoopThread();
-  listenning_ = true;
-  acceptSocket_.listen();
-  acceptChannel_.enableReading();
+    loop_->assertInLoopThread();
+    listenning_ = true;
+    acceptSocket_.listen();
+    acceptChannel_.enableReading();
 }
 
-void Acceptor::handleRead()
+void Acceptor::handleRead() 
 {
-  loop_->assertInLoopThread();
-  InetAddress peerAddr(0);
-  //FIXME loop until no more
-  int connfd = acceptSocket_.accept(&peerAddr);
-  if (connfd >= 0) {
-    if (newConnectionCallback_) {
-      newConnectionCallback_(connfd, peerAddr);
-    } else {
-      sockets::close(connfd);
+    loop_->assertInLoopThread();
+    InetAddress peerAddr(0);
+    // FIXME loop until no more
+    int connfd = acceptSocket_.accept(&peerAddr);
+    if (connfd >= 0) {
+        if (newConnectionCallback_) {
+        newConnectionCallback_(connfd, peerAddr);
+        } else {
+        sockets::close(connfd);
+        }
     }
-  }
 }
-
